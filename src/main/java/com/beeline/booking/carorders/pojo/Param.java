4bc5f45300de1e2d8f1299652ad8064011cce2bf
NIsaev <nisaev@beeline.kg>
2019-12-26 16:54:05 +0600

    Secondary commit

---
+++ src/main/java/com/beeline/booking/carorders/pojo/Param.java
@@ -0,0 +1,23 @@
+package com.beeline.booking.carorders.pojo;
+
+/**
+ * @author NIsaev on 26.12.2019
+ */
+public class Param {
+    private  String error;
+
+    public Param() {
+
+    }
+    public Param(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
